118d41c01d2c0de8f4d210c3a452eacfcb9335e2
Nadim El Taha <nadimeltaha@gmail.com>
2016-02-15 10:50:23 +0100

    Syntax changes

--- index.js
+++ index.js
@@ -6,42 +6,36 @@ const url_parser  = require('url'),
       cluster     = require('cluster'),
       fs          = require('fs'),
       program     = require('commander'),
-      numWorkers  = require('os').cpus().length,
+      numWorkers  = require('os').cpus().length * 2,
       start_time  = Math.floor(new Date())
 
 let urls          = [],
     urls_to_parse = 0,
     num_done      = 0
 
-http.globalAgent.maxSockets = 3096
-
-
 if (cluster.isMaster) {
-  // Parsing arguments
   program
     .version('0.0.1')
-    .parse(process.argv);
+    .parse(process.argv)
 
   if (process.argv.length < 3) {
-    console.error('usage: s3ss_checker <file_with_smooth_streaming_urls_to_check>');
+    console.error('usage: s3ss_checker <file_with_smooth_streaming_urls_to_check>')
     process.exit(1);
   }
 
-  let lineReader  = require('readline').createInterface({
+  let lineReader = require('readline').createInterface({
     input: require('fs').createReadStream(process.argv[2])
-  });
+  })
 
-  lineReader.on('line', function (line) {
-    urls.push(line);
-  });
+  lineReader.on('line', (line) => { urls.push(line) })
 
-  lineReader.on('close', ()=> {
-    urls_to_parse = urls.length;
+  lineReader.on('close', () => {
+    urls_to_parse = urls.length
     const num_per_worker = Math.round(urls_to_parse/numWorkers);
-    console.log(`URLS to parse: ${urls_to_parse} with ${numWorkers} workers`);
-    console.log(`Master cluster setting up ${numWorkers} workers...`);
+    console.log(`URLS to parse: ${urls_to_parse} with ${numWorkers} workers`)
+    console.log(`Master cluster setting up ${numWorkers} workers...`)
 
-    let pointer = 0;
+    let pointer = 0
     for (let i = 0; i < numWorkers; i++) {
       let worker = cluster.fork();
       let to = pointer + num_per_worker;
@@ -52,20 +46,20 @@ if (cluster.isMaster) {
     }
   });
 
-  cluster.on('online', (worker) => { console.log(`Worker ${worker.process.pid} is online!`); });
+  cluster.on('online', (worker) => { console.log(`Worker ${worker.process.pid} is online!`) })
   cluster.on('message', (msg) =>{
-    num_done += 1;
+    num_done++
     if(msg.status !== 200) {
-      console.error(`\nfrom: ${msg.from}, url: ${msg.url}, msg: ${msg.status || msg.error}`);
+      console.error(`\nfrom: ${msg.from}, url: ${msg.url}, msg: ${msg.status || msg.error}`)
     }
-    if (msg.status !== 200 || num_done === urls_to_parse) {
-      let end_time =  Math.floor(new Date()); // timestamp in ms
-      console.log(`\nStarted at: ${start_time}, Finished at: ${end_time}, Duration: ${(end_time - start_time)/1000} secs`);
-      process.exit();
+    if (/*msg.status !== 200 || */num_done === urls_to_parse) {
+      let end_time =  Math.floor(new Date()) // timestamp in ms
+      console.log(`\nStarted at: ${start_time}, Finished at: ${end_time}, Duration: ${(end_time - start_time)/1000} secs`)
+      process.exit()
     }
   });
   cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`);
+    console.log(`Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`)
   });
 }
 
@@ -76,23 +70,25 @@ if (cluster.isWorker) {
       let options = {
         hostname: url_options.hostname,
         path: url_options.path,
-        method: 'HEAD'
+        method: 'HEAD',
+        agent: false
       }
       let req = http.request(options, (res) => {
         process.send({
           status: res.statusCode,
           url: `${url_options.hostname}${url_options.path}`,
           from: process.pid
-        });
-      });
+        })
+      })
+      req.on('data', (data) => { console.log(data) })
       req.on('error', (e) => {
         process.send({
           error: e.message,
           url: `${url_options.hostname}${url_options.path}`,
           from: process.pid
-        });
-      });
-      req.end();
+        })
+      })
+      req.end()
     }
-  });
+  })
 }
